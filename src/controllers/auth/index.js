c65ccd952355ce5f8ec1b91fe2a2b511c44df8ca
Salman <muhsalman54118@gmail.com>
2024-11-08 15:11:18 +0700

    add name auth

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -10,6 +10,7 @@ const router = express.Router();
 const user = new UserModel();
 
 const signUpSchema = Joi.object({
+  fullname : Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string()
     .min(8)
